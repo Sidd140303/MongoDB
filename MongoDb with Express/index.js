e26f38188750b457817d1fd6d4bcd6324781bb67
Siddharth Gupta <siddharth140303@gmail.com>
2025-05-12 18:42:25 +0530

    Chats to DB

--- MongoDb with Express/index.js
+++ MongoDb with Express/index.js
@@ -6,6 +6,8 @@ const Chat = require("./models/chat");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ exteneded: true }));
 
 main().then(res => {
     console.log("Connection Successful...");
@@ -15,13 +17,36 @@ main().then(res => {
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
-app.use(express.static(path.join(__dirname, 'public')));
 
-
+//Index Route
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs", { chats });
+})
+
+//New route
+app.get("/chats/new", async (req, res) => {
+    res.render("newForm.ejs",)
+})
+
+//Create route
+
+app.post("/chats", (req, res) => {
+    let { from, to, msg } = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        createdAt: new Date()
+    })
+    newChat.save().then(res => {
+        console.log("chats were saved");
+    }).catch(err => {
+        console.log(err);
+    })
+    res.redirect("/chats");
+
 })
 
 app.get("/", (req, res) => {
